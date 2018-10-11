1aba480698bb127f6fd6454accc79f697c98b684
乔栋 <QD@cug.edu.cn>
2018-10-11 15:01:03 +0800

    edit batch

--- matlab/batch_generate_cityscapes_PSP_ss.m
+++ matlab/batch_generate_cityscapes_PSP_ss.m
@@ -1,11 +1,11 @@
 class = 'unified';
-sets = {'val', 'train'};
+set = 'val';
 
 keys = [13, 11, 12, 17, 18, 14, 15, 16];
 
 input_list_file = strcat('../cityscapes/splits/', set, '/list.txt');
 output_dir = strcat('../cityscapes/unified/ssMaskFinePSP/', set);
-input_dir = '../cityscapes/gtFine/', set, '/aachen';
+input_dir = strcat('../cityscapes/gtFine/', set);
 
 
 fid = fopen(input_list_file);
@@ -23,7 +23,7 @@ while ischar(input_file)
         mkdir(output_file_dir);
     end
     
-    raw_mask = imread(fullfile(input_dir, [id, '_gtFine_instanceIds.png']));
+    raw_mask = imread(fullfile(input_dir,city, [id, '_gtFine_instanceIds.png']));
     
     [height, width] = size(raw_mask);
     
